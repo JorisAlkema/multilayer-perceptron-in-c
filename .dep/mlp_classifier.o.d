ea49dd56d7cbb9d121ab5e223b75e3b107d9b5ac
Joris Alkema <Jorisdcalkema@gmail.com>
2024-05-01 23:57:55 +0200

    gitignore

---
+++ .dep/mlp_classifier.o.d
@@ -0,0 +1,10 @@
+objdir-CWLITEARM/mlp_classifier.o: mlp_classifier.c mlp_classifier.h \
+ read_csv.h write_csv.h parameters.h simpleserial.h hal.h \
+ ../hardware/victims/firmware/hal/stm32f3/stm32f3_hal.h
+mlp_classifier.h:
+read_csv.h:
+write_csv.h:
+parameters.h:
+simpleserial.h:
+hal.h:
+../hardware/victims/firmware/hal/stm32f3/stm32f3_hal.h:
---
+++ .dep/mlp_trainer.o.d
@@ -0,0 +1,6 @@
+objdir-CWLITEARM/mlp_trainer.o: mlp_trainer.c mlp_trainer.h \
+ forward_propagation.h parameters.h back_propagation.h
+mlp_trainer.h:
+forward_propagation.h:
+parameters.h:
+back_propagation.h:
